65cc262c553d5790ab72193085603e46aeec25f7
Kain <rx1914@hotmail.com>
2024-01-31 01:11:34 -0600

    initial commit

---
+++ src/components/mainCard.js
@@ -0,0 +1,11 @@
+import styles from "./../app/page.module.css";
+
+export const MainCard = () =>{
+    return(
+        <>
+            <h1 className={styles.name}>Cain S. Santiago López</h1>
+            <h2 className={styles.title}>Full stack developer</h2>
+            <h3> Developer with 7+ years of experience in the IT industry.</h3>
+        </>
+    );
+}
